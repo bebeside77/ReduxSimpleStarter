7f32b942c53c916e109e71abebf056564c81f53d
bebeside77 <bebeside77@bebeside77ui-MacBook-Pro.local>
2019-01-26 15:41:06 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
+const API_KEY = 'AIzaSyCOHmnSKDLhBe7aPDdxsvaU4hPBerjtD1o';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+    console.log(data);
+});
+
+// Create a new component. This component should produce
+// some HTML
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SearchBar/>
+			</div>
+		);
+	}
+};
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
